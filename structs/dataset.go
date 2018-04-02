4a684f29a7189eb41271c7672f8433ef76af57bc
Vit Listik <tivvit@seznam.cz>
2018-04-02 11:56:49 +0200

    dataset struct

--- structs/dataset.go
+++ structs/dataset.go
@@ -1,5 +1,21 @@
 package structs
 
 type Dataset struct {
-	Id string `yaml:"id"`
+	Id         string   `yaml:"id"`
+	Data       []string `yaml:"data"`
+	Name       string   `yaml:"name"`
+	Internal   bool     `yaml:"internal"`
+	From       string   `yaml:"from_ds"`
+	Url        string   `yaml:"url"`
+	Maintainer string   `yaml:"maintainer"`
+	Tags       []string `yaml:"tags"`
+	// todo uasage type
+	Usages          []map[string]string `yaml:"usages"`
+	Changelog       [][][]string        `yaml:"changelog"`
+	Markdowns       []string            `yaml:"markdowns"`
+	Characteristics map[string]string   `yaml:"characteristics"`
+	Links           []string            `yaml:"links"`
+	Path            []string            `yaml:"path"`
+	Type            string              `yaml:"type"`
+	Servers         []string            `yaml:"servers"`
 }
